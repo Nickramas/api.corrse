445856baa15b5e0ab32dee5da6cc915bd4d85bd0
Diewald, Niclas-Tim <niclas-tim.diewald@emtec.de>
2020-01-16 12:06:26 +0100

    Add: database connection

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(
+  process.env.DATABASE_PATH,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  err => {
+    if (err) {
+      console.log('Database connection faild', process.env.DATABASE_PATH);
+      return 1;
+    }
+
+    console.log('Connected to database at', process.env.DATABASE_PATH);
+  },
+);
 
 app.get('/', (req, res) => {
   res.send(200);
